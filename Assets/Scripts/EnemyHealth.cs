e77d3da891e44894e302c16ab478cc4f844f62f9
JuanPQ <juan.patino@parqueexplora.org>
2023-08-22 15:58:12 -0500

    Update project

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -5,22 +5,23 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
 
+    [Header ("EnemyHP parameters")]
+
     public int maxHp = 5;
     public int HP = 5;
 
-    bool invincible;
-    float invincibilityTime = 0.8f;
-    float blinkTime = 0.1f;
+    protected bool invincible;
+    protected float invincibilityTime = 0.8f;
+    protected float blinkTime = 0.1f;
 
     public float knockbackStrength = 2f;
-    float knockbackTime = 0.3f;
+    protected float knockbackTime = 0.3f;
 
-    Rigidbody2D rigidbody;
-    SpriteRenderer spriteRenderer;
-    EnemyHit enemyHit;
+    protected Rigidbody2D rigidbody;
+    protected SpriteRenderer spriteRenderer;
+    protected EnemyHit enemyHit;
 
-
-    void Start()
+    public virtual void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -30,13 +31,14 @@ public class EnemyHealth : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Weapon" && !invincible)
+        if (collision.CompareTag("Weapon") && !invincible)
         {
             HP--;
             if (HP <= 0)
             {
                 enemyHit.Defeat();
             }
+            StopBehaviour();
             StartCoroutine(Invincibility());
             StartCoroutine(Knockback(collision.transform.position));
         }
@@ -60,10 +62,16 @@ public class EnemyHealth : MonoBehaviour
     }
     IEnumerator Knockback(Vector3 hitPosition)
     {
-        if (knockbackStrength <= 0) yield break;
+        if (knockbackStrength <= 0) 
+        {
+            if(HP > 0)ContinueBehaviour(); 
+            yield break;
+        }
         rigidbody.velocity = (transform.position - hitPosition).normalized * knockbackStrength;
         yield return new WaitForSeconds(knockbackTime);
         rigidbody.velocity = Vector3.zero;
+        yield return new WaitForSeconds(knockbackTime);
+        if(HP > 0)ContinueBehaviour(); 
     }
 
     public void HideEnemy()
@@ -71,5 +79,15 @@ public class EnemyHealth : MonoBehaviour
         StopAllCoroutines();
         rigidbody.velocity = Vector3.zero;
         spriteRenderer.enabled = false;
+    }
+
+    public virtual void StopBehaviour()
+    {
+
+    }
+
+    public virtual void ContinueBehaviour()
+    {
+        
     }
 }
